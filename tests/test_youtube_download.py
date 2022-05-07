5660e7cf94eb4e8d0a04bf4129612582385c7bdd
Samuel Wu 吴申榕 <twopizza9621536@gmail.com>
2022-05-07 09:52:59 -0400

    Remove classes for unittest migration in pytest
    Signed-off-by: Samuel Wu 吴申榕 <twopizza9621536@gmail.com>

--- tests/test_youtube_download.py
+++ tests/test_youtube_download.py
@@ -1,7 +1,9 @@
 # SPDX-FileCopyrightText: 2022 The Social Media Catalog Contributors
 
 # SPDX-License-Identifier: BSD-3-Clause
-"""Unit Test for downloading video metadata."""
+"""Unit Test for downloading video metadata and to check if the downloader is
+working as intended.
+"""
 
 from pytest import raises
 import json
@@ -13,41 +15,29 @@ from social_media_catalog.youtube import Video
 
 TEST_PLAYLIST_DATA = "playlist.json"
 PLAYLIST_ID = "PLFsQleAWXsj_4yDeebiIADdH5FMayBiJo"
+def test_get_playlist() -> None:
+    """Check if it can download an entire playlist and if the list is
+    correct.
+    """
 
+    vd = VideoDownloader()
+    vd.auth_credentials = get_youtube_credentials()
+    vd.playlist_id = PLAYLIST_ID
+    playlist = Playlist(PLAYLIST_ID, "important videos", vd.get_playlist())
 
-class TestYoutubeDownload:
-    """Unit tests to check if the downloader is working as intended."""
+    with open(TEST_PLAYLIST_DATA, encoding="utf-8") as test_file:
+        json_data = json.loads(test_file.read())
 
-    def test_get_playlist(self) -> None:
-        """Check if it can download an entire playlist and if the list is
-        correct.
+    Playlist.from_json(json_data)
+    assert json.dumps(playlist.to_dict()), json.dumps(json_data)
 
-        Args:
-            self: The test object to assert if the list is correct.
-        """
+def test_object_exceptions() -> None:
+    """Test exceptions if there are missing keys or values."""
+    dict_to_video_data = {"title": "Testing 123"}
+    dict_to_playlist_data = {"playlistId": "ABC1DEF2GHI3JKL4MNO5"}
 
-        vd = VideoDownloader()
-        vd.auth_credentials = get_youtube_credentials()
-        vd.playlist_id = PLAYLIST_ID
-        playlist = Playlist(PLAYLIST_ID, "important videos", vd.get_playlist())
+    with raises(ValueError):
+        Video.from_json(dict_to_video_data)
 
-        with open(TEST_PLAYLIST_DATA, encoding="utf-8") as test_file:
-            json_data = json.loads(test_file.read())
-
-        Playlist.from_json(json_data)
-        assert json.dumps(playlist.to_dict()), json.dumps(json_data)
-
-    def test_object_exceptions(self) -> None:
-        """Test exceptions if there are missing keys or values.
-
-        Args:
-            self: The test object to test exceptions in 'video_downloader'.
-        """
-        dict_to_video_data = {"title": "Testing 123"}
-        dict_to_playlist_data = {"playlistId": "ABC1DEF2GHI3JKL4MNO5"}
-
-        with raises(ValueError):
-            Video.from_json(dict_to_video_data)
-
-        with raises(ValueError):
-            Playlist.from_json(dict_to_playlist_data)
+    with raises(ValueError):
+        Playlist.from_json(dict_to_playlist_data)
